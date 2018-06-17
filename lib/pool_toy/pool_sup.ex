4f2943a19a13f2d28912b5028c62f8f182cddcd7
David Sulc <davidsulc@gmail.com>
2018-06-17 17:44:21 +0200

    have PoolSup start PoolMan

--- lib/pool_toy/pool_sup.ex
+++ lib/pool_toy/pool_sup.ex
@@ -3,12 +3,13 @@ defmodule PoolToy.PoolSup do
 
   @name __MODULE__
 
-  def start_link() do
-    Supervisor.start_link(__MODULE__, [], name: @name)
+  def start_link(args) when is_list(args) do
+    Supervisor.start_link(__MODULE__, args, name: @name)
   end
 
-  def init([]) do
-    children = []
+  def init(args) do
+    pool_size = args |> Keyword.fetch!(:size)
+    children = [{PoolToy.PoolMan, pool_size}]
 
     Supervisor.init(children, strategy: :one_for_all)
   end
