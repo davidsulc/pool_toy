412b87c76f2b185d2a329c7e532cc1407d6e4732
David Sulc <davidsulc@gmail.com>
2018-06-24 15:12:23 +0200

    implemented worker checkin/checkout

--- lib/pool_toy/pool_man.ex
+++ lib/pool_toy/pool_man.ex
@@ -11,9 +11,29 @@ defmodule PoolToy.PoolMan do
     GenServer.start_link(__MODULE__, size, name: @name)
   end
 
+  def checkout() do
+    GenServer.call(@name, :checkout)
+  end
+
+  def checkin(worker) do
+    GenServer.cast(@name, {:checkin, worker})
+  end
+
   def init(size) do
     send(self(), :start_workers)
     {:ok, %State{size: size}}
+  end
+
+  def handle_call(:checkout, _from, %State{workers: []} = state) do
+    {:reply, :full, state}
+  end
+
+  def handle_call(:checkout, _from, %State{workers: [worker | rest]} = state) do
+    {:reply, worker, %{state | workers: rest}}
+  end
+
+  def handle_cast({:checkin, worker}, %State{workers: workers} = state) do
+    {:noreply, %{state | workers: [worker | workers]}}
   end
 
   def handle_info(:start_workers, %State{size: size} = state) do
